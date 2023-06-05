741058a8e2946e854f799bae5c81b56354502536
HubTicaBB <tijana.ilic@volvocars.com>
2023-06-05 12:34:34 +0200

    Implement single pass calculation

--- CongestionTaxCalculator/CalculatorTests/PrintTotalAmountTests.cs
+++ CongestionTaxCalculator/CalculatorTests/PrintTotalAmountTests.cs
@@ -1,11 +1,101 @@
+using CongestionTaxCalculator;
+
 namespace CalculatorTests
 {
     [TestClass]
     public class PrintTotalAmountTests
     {
-        [TestMethod]
-        public void GivenX_DoesY()
+        public static IEnumerable<object[]> SinglePassInterval8 => new[]
         {
+            new object[] { "2023-05-31 06:00" },
+            new object[] { "2023-05-31 06:29" },
+            new object[] { "2023-05-31 08:30" },
+            new object[] { "2023-05-31 14:59" },
+            new object[] { "2023-05-31 18:00" },
+            new object[] { "2023-05-31 18:29" },
+        };
+
+        public static IEnumerable<object[]> SinglePassInterval13 => new[]
+        {
+            new object[] { "2023-05-31 06:30" },
+            new object[] { "2023-05-31 06:59" },
+            new object[] { "2023-05-31 08:00" },
+            new object[] { "2023-05-31 08:29" },
+            new object[] { "2023-05-31 15:00" },
+            new object[] { "2023-05-31 15:29" },
+            new object[] { "2023-05-31 17:00" },
+            new object[] { "2023-05-31 17:59" },
+        };
+
+        public static IEnumerable<object[]> SinglePassInterval18 => new[]
+        {
+            new object[] { "2023-05-31 07:00" },
+            new object[] { "2023-05-31 07:59" },
+            new object[] { "2023-05-31 15:30" },
+            new object[] { "2023-05-31 16:59" },
+        };
+
+        public static IEnumerable<object[]> SinglePassIntervalFree => new[]
+{
+            new object[] { "2023-05-31 18:30" },
+            new object[] { "2023-05-31 05:59" },
+        };
+
+
+        [TestMethod]
+        [DynamicData(nameof(SinglePassInterval8))]
+        public void GivenSinglePassInInterval8_Prints8(string pass)
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+            var expected = "The total fee is 8 kr";
+
+            Calculator.PrintTotalAmount(pass);
+            var actuall = stringWriter.ToString().Trim();
+
+            Assert.AreEqual(expected, actuall);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SinglePassInterval13))]
+        public void GivenSinglePassInInterval13_Prints13(string pass)
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+            var expected = "The total fee is 13 kr";
+
+            Calculator.PrintTotalAmount(pass);
+            var actuall = stringWriter.ToString().Trim();
+
+            Assert.AreEqual(expected, actuall);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SinglePassInterval18))]
+        public void GivenSinglePassInInterval18_Prints18(string pass)
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+            var expected = "The total fee is 18 kr";
+
+            Calculator.PrintTotalAmount(pass);
+            var actuall = stringWriter.ToString().Trim();
+
+            Assert.AreEqual(expected, actuall);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SinglePassIntervalFree))]
+        public void GivenSinglePassInIntervalFree_Prints0(string pass)
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+            var expected = "The total fee is 0 kr";
+
+            Calculator.PrintTotalAmount(pass);
+            var actuall = stringWriter.ToString().Trim();
+
+            Assert.AreEqual(expected, actuall);
         }
     }
 }
--- CongestionTaxCalculator/CongestionTaxCalculator/Calculator.cs
+++ CongestionTaxCalculator/CongestionTaxCalculator/Calculator.cs
@@ -1,9 +1,44 @@
-﻿namespace CongestionTaxCalculator;
+﻿using System.Globalization;
+
+namespace CongestionTaxCalculator;
 
 public class Calculator
 {
     public static void PrintTotalAmount(string tollStationPasses)
     {
-        throw new NotImplementedException();
+        var dateTime = DateTime.Parse(tollStationPasses);
+        var totalFee = GetPassFee(dateTime);        
+
+        Console.WriteLine($"The total fee is {totalFee} kr");
+    }
+
+    private static object GetPassFee(DateTime dateTime)
+    {
+        switch (dateTime.Hour)
+        {
+            case 6:
+                return dateTime.Minute <= 29 ? 8 : 13;
+            case 7:
+                return 18;
+            case 8:
+                return dateTime.Minute <= 29 ? 13 : 8;
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+                return 8;
+            case 15:
+                return dateTime.Minute <= 29 ? 13 : 18;
+            case 16:
+                return 18;
+            case 17:
+                return 13;
+            case 18:
+                return dateTime.Minute <= 29 ? 8 : 0;
+            default:
+                return 0;
+        }
     }
 }
