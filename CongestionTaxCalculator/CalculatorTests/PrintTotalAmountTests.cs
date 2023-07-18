3db22fb9eef1fcc8bb4284fc1cf3fd62db33776f
HubTicaBB <tijana.ilic@volvocars.com>
2023-07-18 15:50:23 +0200

    Task 1

--- CongestionTaxCalculator/CalculatorTests/PrintTotalAmountTests.cs
+++ CongestionTaxCalculator/CalculatorTests/PrintTotalAmountTests.cs
@@ -1,11 +1,20 @@
+using CongestionTaxCalculator;
+
 namespace CalculatorTests
 {
     [TestClass]
     public class PrintTotalAmountTests
     {
         [TestMethod]
-        public void GivenX_DoesY()
+        public void PrintTotalAmount_PrintsCorrectAmount()
         {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+
+            Calculator.PrintTotalAmount("2023-05-31 17:45");
+            var actual = stringWriter.ToString().Trim();
+
+            Assert.AreEqual("The total fee is 13 kr", actual);
         }
     }
 }
--- CongestionTaxCalculator/CongestionTaxCalculator/Calculator.cs
+++ CongestionTaxCalculator/CongestionTaxCalculator/Calculator.cs
@@ -1,9 +1,51 @@
-﻿namespace CongestionTaxCalculator;
+﻿using System;
+
+namespace CongestionTaxCalculator;
 
 public class Calculator
 {
     public static void PrintTotalAmount(string tollStationPasses)
     {
-        throw new NotImplementedException();
+        var parsedTollStationPass = DateTime.Parse(tollStationPasses);
+
+        int totalFee;
+
+        switch (parsedTollStationPass.Hour)
+        {
+            case 6:
+                totalFee = parsedTollStationPass.Minute <= 29 ? 8 : 13;
+                break;
+            case 7:
+                totalFee = 18;
+                break;
+            case 8:
+                totalFee = parsedTollStationPass.Minute <= 29 ? 13 : 8;
+                break;
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+                totalFee = 8;
+                break;
+            case 15:
+                totalFee = parsedTollStationPass.Minute <= 29 ? 13 : 18;
+                break;
+            case 16:
+                totalFee = 18;
+                break;
+            case 17:
+                totalFee = 13;
+                break;
+            case 18:
+                totalFee = parsedTollStationPass.Minute <= 29 ? 8 : 0;
+                break;
+            default:
+                totalFee = 0;
+                break;
+        }
+
+        Console.WriteLine($"The total fee is {totalFee} kr");
     }
 }
